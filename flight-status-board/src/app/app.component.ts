41e7e82ecdf0fd28f65d812e1cd4ac33f2777717
Carolista <cjones.renaissancewoman@gmail.com>
2021-07-01 20:41:45 -0500

    added filter and reset functionality

--- flight-status-board/src/app/app.component.ts
+++ flight-status-board/src/app/app.component.ts
@@ -14,9 +14,13 @@ export class AppComponent {
 
     allArrivals: Flight[] = [];
     allDepartures: Flight[] = [];
+    filteredArrivals: Flight[] = [];
+    filteredDepartures: Flight[] = [];
 
     arrivalText: string = "arrival";
     departureText: string = "departure";
+
+    showArrivals: boolean = true; 
 
     getFlights(): void {
         fetch("assets/data/flights.json").then((response) => {
@@ -31,9 +35,37 @@ export class AppComponent {
                         this.allDepartures.push(newFlight);
                     }
                 })
-                console.log(json);
+                this.filteredArrivals = [...this.allArrivals];
+                this.filteredDepartures = [...this.allDepartures];
             });
         });
+    }
+
+    filterFlights(keyword: string): void {
+        keyword = keyword.toLowerCase();
+        let matchingArrivals: Flight[] = [];
+        for(let i=0; i < this.allArrivals.length; i++) {
+            let flight = this.allArrivals[i];
+            let flightInfo = (flight.airline + flight.flightNo + flight.origin + flight.status).toLowerCase();
+            if (flightInfo.indexOf(keyword) >= 0) {
+                matchingArrivals.push(flight);
+            }            
+        }
+        this.filteredArrivals = [...matchingArrivals];
+        let matchingDepartures: Flight[] = [];
+        for(let i=0; i < this.allDepartures.length; i++) {
+            let flight = this.allDepartures[i];
+            let flightInfo = (flight.airline + flight.flightNo + flight.destination + flight.status).toLowerCase();
+            if (flightInfo.indexOf(keyword) >= 0) {
+                matchingDepartures.push(flight);
+            }
+        }
+        this.filteredDepartures = [...matchingDepartures];
+    }
+
+    resetFlights(): void {
+        this.filteredArrivals = [...this.allArrivals];
+        this.filteredDepartures = [...this.allDepartures];
     }
 
 }
